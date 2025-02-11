aec6e3ced674037aa2684c268912c2800d94ab51
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-02-11 22:02:47 +0100

    Added WorldType, WorldSize and WorldSubType

---
+++ GeneratorLibrary/Models/WorldType.cs
@@ -0,0 +1,28 @@
+﻿namespace GeneratorLibrary.Models
+{
+    public record WorldType(WorldSize Size, WorldSubType SubType);
+
+    public enum WorldSize
+    {
+        Tiny,
+        Small,
+        Standard,
+        Large,
+        Special // For Asteroid Belt and Gas Giant
+    }
+
+    public enum WorldSubType
+    {
+        Chthonian,
+        Greenhouse,
+        Hadean,
+        Ice,
+        Rock,
+        Sulfur,
+        Ammonia,
+        Ocean,
+        Garden,
+        AsteroidBelt,
+        GasGiant
+    }
+}
