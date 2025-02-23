80ed69c68bb7f5841e7e5e7e956d2cbe0a4f70b3
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-02-23 22:56:30 +0100

    Added Tests for Diameter and Surface Gravity

--- GeneratorLibrary.Tests/Generators/Tables/ChartacteristicsTablesTests.cs
+++ GeneratorLibrary.Tests/Generators/Tables/ChartacteristicsTablesTests.cs
@@ -31,5 +31,35 @@ namespace GeneratorLibrary.Tests.Generators.Tables
             // Assert
             Assert.InRange(density, minDensity, maxDensity);
         }
+
+        [Theory]
+        [InlineData(WorldSize.Large, 288, 0.9, 0.065, 0.091)]
+        [InlineData(WorldSize.Standard, 255, 1.0, 0.030, 0.065)]
+        [InlineData(WorldSize.Small, 200, 0.8, 0.024, 0.030)]
+        [InlineData(WorldSize.Tiny, 150, 0.5, 0.004, 0.024)]
+        public void GenerateWorldDiameter_ShouldReturnValidRange(
+            WorldSize size, double blackbodyTemperature, double density, double minSize, double maxSize)
+        {
+            // Act
+            double result = CharacteristicsTables.GenerateWorldDiameter(size, blackbodyTemperature, density);
+
+            // Assert
+            Assert.InRange(result, Math.Sqrt(blackbodyTemperature / density) * minSize,
+                                     Math.Sqrt(blackbodyTemperature / density) * maxSize);
+        }
+
+        [Theory]
+        [InlineData(0.5, 0.9, 0.45)]
+        [InlineData(1.0, 1.0, 1.00)]
+        [InlineData(1.2, 0.8, 0.96)]
+        [InlineData(0.3, 1.2, 0.36)]
+        public void GenerateWorldSurfaceGravity_ShouldReturnExpectedValue(double diameter, double density, double expected)
+        {
+            // Act
+            double result = CharacteristicsTables.GenerateWorldSurfaceGravity(diameter, density);
+
+            // Assert
+            Assert.Equal(expected, result, precision: 2);
+        }
     }
 }
