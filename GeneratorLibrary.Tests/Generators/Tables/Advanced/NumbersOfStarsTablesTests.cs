2e18ed11f6e7b34df71717326829cffa7d91cadc
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-03-29 15:21:20 +0100

    Fixed S4144 on tests for NumbersOfStars

--- GeneratorLibrary.Tests/Generators/Tables/Advanced/NumbersOfStarsTablesTests.cs
+++ GeneratorLibrary.Tests/Generators/Tables/Advanced/NumbersOfStarsTablesTests.cs
@@ -5,50 +5,50 @@ namespace GeneratorLibrary.Tests.Generators.Tables.Advanced
     public class NumbersOfStarsTablesTests
     {
         [Theory]
-        [InlineData(3, 1)]
-        [InlineData(4, 1)]
-        [InlineData(5, 1)]
-        [InlineData(6, 1)]
-        [InlineData(7, 1)]
-        [InlineData(8, 1)]
-        [InlineData(9, 1)]
-        [InlineData(10, 1)]
-        public void Roll_3_to_10_Returns_1Star(int roll, int expectedStars)
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void Roll_3_to_10_Returns_1Star(int roll)
         {
             // Act
             int stars = NumberOfStarsTable.GenerateNumberOfStars(roll);
 
             // Assert
-            Assert.Equal(expectedStars, stars);
+            Assert.Equal(1, stars);
         }
 
         [Theory]
-        [InlineData(11, 2)]
-        [InlineData(12, 2)]
-        [InlineData(13, 2)]
-        [InlineData(14, 2)]
-        [InlineData(15, 2)]
-        public void Roll_11_to_15_Returns_2Stars(int roll, int expectedStars)
+        [InlineData(11)]
+        [InlineData(12)]
+        [InlineData(13)]
+        [InlineData(14)]
+        [InlineData(15)]
+        public void Roll_11_to_15_Returns_2Stars(int roll)
         {
             // Act
             int stars = NumberOfStarsTable.GenerateNumberOfStars(roll);
 
             // Assert
-            Assert.Equal(expectedStars, stars);
+            Assert.Equal(2, stars);
         }
 
         [Theory]
-        [InlineData(16, 3)]
-        [InlineData(17, 3)]
-        [InlineData(18, 3)]
-        [InlineData(int.MaxValue, 3)]
-        public void Roll_16_or_more_Returns_3Stars(int roll, int expectedStars)
+        [InlineData(16)]
+        [InlineData(17)]
+        [InlineData(18)]
+        [InlineData(int.MaxValue)]
+        public void Roll_16_or_more_Returns_3Stars(int roll)
         {
             // Act
             int stars = NumberOfStarsTable.GenerateNumberOfStars(roll);
 
             // Assert
-            Assert.Equal(expectedStars, stars);
+            Assert.Equal(3, stars);
         }
 
         [Theory]
